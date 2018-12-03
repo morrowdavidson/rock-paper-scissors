6de39791e5ecb9b6202e955537a58a199f52b36a
Morrow Davidson <morrow.davidson@gmail.com>
2018-12-02 22:51:54 -0800

    Allow to play best 3/5

--- main.js
+++ main.js
@@ -9,16 +9,47 @@ function computerPlay() {
     }
 }
 
+let playerScore = 0;
+let computerScore = 0;
+let currentScoreBoard = "| Score | You: 0 | Computer: 0";
+
+function resetScore(){
+    playerScore = 0;
+    computerScore = 0;
+}
+
 function playRound(playerSelection, computerSelection){
     if (playerSelection == computerSelection){
-        alert("Tie");
-        startPlay();
+        alert("Tie" + " " + currentScoreBoard);
     } else if ((playerSelection == "rock" && computerSelection == "scissors") || (playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper")) {
-        alert("You win: " + playerSelection + " beats " + computerSelection);
-        startPlay();
+        scoreKeeper("player won");
+        alert("You win: " + playerSelection + " beats " + computerSelection + " " + currentScoreBoard);
+       
+        if (playerScore == 3){
+            resetScore();
+            alert("Victory: You won 3/5");
+        }
+            
     } else {
-        alert("You loose: " + computerSelection + " beats " + playerSelection);
-        startPlay();
+        scoreKeeper("player lost");
+        alert("You loose: " + computerSelection + " beats " + playerSelection + " " + currentScoreBoard);
+        
+        if (computerScore == 3){
+            resetScore();
+            alert("Defeat: You lost 3/5");    
+        }
+        
+    }
+    startPlay();
+}
+
+function scoreKeeper(result){
+    if (result == "player won"){
+        ++playerScore;        
+        return currentScoreBoard = "| Score | You: " + playerScore + " | Computer: " + computerScore;
+    } else {
+        ++computerScore;
+        return currentScoreBoard = "| Score | You: " + playerScore + " | Computer: " + computerScore;
     }
 }
 
