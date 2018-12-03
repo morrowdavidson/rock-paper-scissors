c431d15c8b5e512bfd51a232b141511f91d9cd5c
Morrow Davidson <morrow.davidson@gmail.com>
2018-12-02 21:26:04 -0800

    Add function to return 'Rock Paper or Scissors'

---
+++ main.js
@@ -0,0 +1,12 @@
+function computerPlay() {
+    let randomNumber = Math.random();
+    if (randomNumber < (1/3)){
+        return "rock";
+    } else if (randomNumber < (2/3)){
+        return "paper";
+    }else {
+        return "scissors";
+    }
+}
+
+
